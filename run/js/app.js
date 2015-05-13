4e62229ce6a0c268d9ca244f8b9e33dce1234839
Matt Galvin <matt@carvoyant.com>
2015-05-13 18:15:04 -0400

    Added runtime files

---
+++ run/js/app.js
@@ -0,0 +1,39 @@
+
+var carvoyantConfig = angular.module('carvoyantConfig', []);
+
+carvoyantConfig.constant({
+	carvoyantBaseApi: 'https://api.carvoyant.com/v1/api/',
+	carvoyantOAuth2Uri: 'http://localhost:8080/AuthenticationServer/oauth/authorize',
+	carvoyantClientId: 'c9y7z9bqh6ntqfna56a6xp4p',
+	googleMapsKey: 'AIzaSyBfO4TcUaBaeMoIkUODVbBEf1tjE47rZ14'
+});
+
+var carvoyantApp = angular.module('carvoyantClient', [
+	'ngRoute',
+	'carvoyantConfig',
+	'carvoyantControllers',
+	'carvoyantService'
+]);
+
+carvoyantApp.factory('UserInfo', function () {
+	return {
+		account: null,
+		vehicles: null
+	};
+});
+
+carvoyantApp.config(['$routeProvider', function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+		templateUrl: 'templates/login.html',
+		controller: 'LoginController'
+	})
+		.when('/authorized', {
+		template: '<div>Authorized</div>',
+		controller: 'AuthorizedController'
+	})
+		.when('/vehicles', {
+		templateUrl: 'templates/vehicles.html',
+		controller: 'CarvoyantController'
+	});
+}]);
